8a47a033c8178aada51c96e2626cb767481ceac3
Darrell Martin <darrell@briarmedia.biz>
2026-03-22 17:35:32 +0000

    [ALL] Updated ALiVE version to 2.3.0.2603222

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -6,7 +6,7 @@
 #define MINOR 3
 #define PATCHLVL 0
 // BUILD should be YYMMDDR eg 1311211
-#define BUILD 2603221
+#define BUILD 2603222
 
 #define CLUSTERBUILD "Arma 3","Arma3",220,152984,"Stable"
 
